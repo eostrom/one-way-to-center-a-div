fbf74984074b5c1a2970f91005d7c62e252420dd
Erik Ostrom <code@erikostrom.com>
2021-11-21 22:46:08 -0600

    Working locally.
    Built this thing on StackBlitz, who knows what the differences are.

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,15 +1,4 @@
-import cookie from 'cookie';
-import { v4 as uuid } from '@lukeed/uuid';
-import type { GetContext, Handle } from '@sveltejs/kit';
-
-export const getContext: GetContext = (request) => {
-	const cookies = cookie.parse(request.headers.cookie || '');
-
-	return {
-		is_new: !cookies.userid,
-		userid: cookies.userid || uuid()
-	};
-};
+import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ request, render }) => {
 	// TODO https://github.com/sveltejs/kit/issues/1046
@@ -17,20 +6,6 @@ export const handle: Handle = async ({ request, render }) => {
 		...request,
 		method: (request.query.get('_method') || request.method).toUpperCase()
 	});
-
-	const { is_new, userid } = request.context;
-
-	if (is_new) {
-		// if this is the first time the user has visited this app,
-		// set a cookie so that we recognise them when they return
-		return {
-			...response,
-			headers: {
-				...response.headers,
-				'set-cookie': `userid=${userid}; Path=/; HttpOnly`
-			}
-		};
-	}
 
 	return response;
 };
